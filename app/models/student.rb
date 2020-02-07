e06043a6774021f8967517e034be777260e0e397
Eddy Tyannikov <etyannikov@gmail.com>
2020-02-07 21:59:57 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
+
 require 'pry'
 class Student < ActiveRecord::Base
   def to_s
     first_name << " " << last_name
   end
-=======
-class Student
-  
->>>>>>> 8e994eef26440e96c2c1ddc3b05736b9e0aa884e
 end
